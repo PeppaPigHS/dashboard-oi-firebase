942bffa05b2a49688e5ae3f619bb32264460b8a3
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-18 20:10:12 +0700

    Change function to onCall

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,17 +3,21 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp()
 
-export const onRegister = functions
+export const checkUser = functions
   .region('asia-east2')
-  .auth.user()
-  .onCreate(
-    async (user: admin.auth.UserRecord, context: functions.EventContext) => {
+  .https.onCall(
+    async (requestData: any, context: functions.https.CallableContext) => {
+      if (!context.auth) return false
       try {
-        const uid = user.uid
-        await admin
-          .firestore()
-          .doc(`users/${uid}`)
-          .set({ username: user.displayName, checklist: [] })
+        const uid = context.auth.uid
+        const userRef = admin.firestore().doc(`users/${uid}`)
+        if (!(await userRef.get()).exists) {
+          await userRef.set({
+            username: requestData.displayName,
+            checklist: [],
+          })
+        }
+        return true
       } catch (error) {
         throw new functions.https.HttpsError('unknown', error)
       }
